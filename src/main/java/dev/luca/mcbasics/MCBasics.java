1d1be578c0e7c0341cbc2a876799b5aafc62b3c6
luca <luca@skydb.de>
2026-01-23 21:10:00 +0100

    Add Minecraft Auto-Updater deployment API
    - Added autodeployment-minecraft dependency
    - Added shading and relocation for deployment library
    - Initialized MinecraftSelfUpdater in onEnable with project credentials
    - Project UUID: 220e4f0e-8163-4d91-b7bc-7ea1c6ab4264

--- src/main/java/dev/luca/mcbasics/MCBasics.java
+++ src/main/java/dev/luca/mcbasics/MCBasics.java
@@ -2,16 +2,23 @@ package dev.luca.mcbasics;
 
 import dev.luca.mcbasics.api.Message;
 import dev.luca.mcbasics.commands.*;
+import top.insights.deployment.MinecraftSelfUpdater;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCBasics extends JavaPlugin {
 
     private static MCBasics instance;
 
+    private static final String PROJECT_UUID = "220e4f0e-8163-4d91-b7bc-7ea1c6ab4264";
+    private static final String PROJECT_KEY = "xTesBisS8Tc7luCqdOWGs88dcDoetuIw";
+
     @Override
     public void onEnable() {
         instance = this;
         Message.init(this);
+
+        MinecraftSelfUpdater.start(this, PROJECT_UUID, PROJECT_KEY, true);
+
         getLogger().info("MCBasics plugin enabled!");
         getLogger().info("Loaded " + dev.luca.minecraftapi.MessageAPI.getLoadedMessageCount() + " messages");
         registerCommands();
